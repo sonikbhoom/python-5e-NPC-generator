d7c3fa0c6d2c7e964cf7d888e1a865f19f89dc93
sonikbhoom <sonik.bhoom@bell.net>
2021-03-21 15:19:18 -0400

    ran code through pylinter for comformity ... beep boop u will CONFORM

--- generate_character.py
+++ generate_character.py
@@ -1,23 +1,25 @@
+""" Generate NPC character stats in python """
 import random
-import json
 import sys
 from collections import OrderedDict
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+class Colors:
+    """ print formatting class """
+    c_header = '\033[95m'
+    ok_blue = '\033[94m'
+    ok_green = '\033[92m'
+    _warning = '\033[93m'
+    _fail = '\033[91m'
+    end_c = '\033[0m'
 
     def disable(self):
-        self.HEADER = ''
-        self.OKBLUE = ''
-        self.OKGREEN = ''
-        self.WARNING = ''
-        self.FAIL = ''
-        self.ENDC = ''
+        """ self """
+        self.c_header = ''
+        self.ok_blue = ''
+        self.ok_green = ''
+        self._warning = ''
+        self._fail = ''
+        self.end_c = ''
 
 char_maps = {}
 char_maps['archer']    = ['dex','con','wis','cha','int','str'] # Assuming Dex based Fighter
@@ -25,7 +27,7 @@ char_maps['barbarian'] = ['str','con','dex','wis','cha','int']
 char_maps['bard']      = ['cha','dex','con','int','wis','str']
 char_maps['cleric']    = ['wis','con','dex','str','int','cha']
 char_maps['druid']     = ['wis','con','dex','int','str','cha']
-char_maps['eknight']   = ['str','con','int','wis','cha','dex'] # Assuming Dex based Fighter with tweak
+char_maps['eknight']   = ['str','con','int','wis','cha','dex'] # Assuming Dex based Fighter
 char_maps['fighter']   = ['str','con','wis','cha','int','dex'] # Assuming Strength-Based Melee
 char_maps['monk']      = ['dex','con','wis','str','int','cha']
 char_maps['paladin']   = ['str','cha','con','wis','int','dex']
@@ -36,72 +38,67 @@ char_maps['srocerer']  = ['cha','con','dex','int','wis','str']
 char_maps['warlock']   = ['cha','con','dex','int','wis','str']
 char_maps['wizard']    = ['int','con','dex','wis','cha','str']
 
-stat_max = 108
-stat_total = 0
+STAT_MAX = 108
 
-# character model with primary stats ordered
 def character_ordered(_stats, _stats_map):
+    """ load character model with primary ordered stats """
     _character = OrderedDict()
     for stat in _stats_map:
         _character[stat] = _stats.pop(0)
     return _character
 
 def genstats():
+    """ main stats generator function; the main math! """
     stats = sorted(([sum(sorted([random.randint(1,6) for _ in range (4)])[1:]) for _ in range(6)]),reverse=True)
     return stats
 
 def nice_print(character):
-    # pretty up the presentation of the character stats
-    # print(character)
+    """ pretty up the presentation of the character stats """
     limiter = ''
     stat_total = (character['str'] + character['dex'] + character['con'] + character['int'] + character['wis'] + character['cha'] )
-    if (stat_total <= 64):
+    if stat_total <= 64:
         limiter = 'weak'
-    elif ((stat_total > 64) and (stat_total < 80)):
+    elif (stat_total > 64) and (stat_total < 80):
         limiter = 'medium'
-    elif (stat_total >= 80):
+    elif stat_total >= 80:
         limiter = 'strong'
-    elif (stat_total > 100):
+    elif stat_total > 100:
         limiter = 'epic'
-    print("%sSTR%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,character['str'],bcolors.ENDC))
-    print("%sDEX%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,character['dex'],bcolors.ENDC))
-    print("%sCON%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,character['con'],bcolors.ENDC))
-    print("%sINT%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,character['int'],bcolors.ENDC))
-    print("%sWIS%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,character['wis'],bcolors.ENDC))
-    print("%sCHA%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,character['cha'],bcolors.ENDC))
-    print("%sTotal Points%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,stat_total,bcolors.ENDC))
-    print("%sStats Rating%s : %s%s%s" % (bcolors.OKBLUE,bcolors.ENDC,bcolors.OKGREEN,limiter,bcolors.ENDC))
+    print("%sSTR%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,character['str'],Colors.end_c))
+    print("%sDEX%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,character['dex'],Colors.end_c))
+    print("%sCON%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,character['con'],Colors.end_c))
+    print("%sINT%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,character['int'],Colors.end_c))
+    print("%sWIS%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,character['wis'],Colors.end_c))
+    print("%sCHA%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,character['cha'],Colors.end_c))
+    print("%sTotal Points%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,stat_total,Colors.end_c))
+    print("%sStats Rating%s : %s%s%s" % (Colors.ok_blue,Colors.end_c,Colors.ok_green,limiter,Colors.end_c))
     print("")
 
 def main(character_type):
-
-    # generate character stats
+    """ generate character stats """
     character_stats = genstats()
-
-    # TODO: allow for class variants (e.g. Heavy armour cleric, and dex based paldin)
 
     try:
         character = character_ordered(character_stats,char_maps[character_type])
 
-    except KeyError:    
-        # assign stats to character abilities
-        print ("%s%s%s %s%s" % (bcolors.OKGREEN, character_type, bcolors.OKBLUE,"class not found. assuming wizard\n",bcolors.ENDC))
+    except KeyError:
+        print ("%s%s%s %s%s" % (Colors.ok_green, character_type, Colors.ok_blue,"class not found. assuming wizard\n",Colors.end_c))
         character_type = 'wizard'
         character = character_ordered(character_stats,char_maps[character_type])
 
-    print(bcolors.OKBLUE + "Stats created for" + bcolors.OKGREEN, character_type, bcolors.ENDC)
+    print(Colors.ok_blue + "Stats created for" + Colors.ok_green, character_type, Colors.end_c)
     nice_print(character)
 
-if (__name__ == '__main__'):
-    print (bcolors.HEADER + "Generate stats for NPC with class" + bcolors.ENDC)
-    print (bcolors.HEADER + "Version: 0.6  Author: Nim Turen\n" + bcolors.ENDC)
+if __name__ == '__main__':
+    print ("%s%s%s" % (Colors.c_header,"Generate stats for NPC with class",Colors.end_c))
+    print ("%s%s%s" % (Colors.c_header,"Version: 0.6  Author: Nim Turen\n",Colors.end_c))
     try:
         _character_type = sys.argv[1].lower()
-    except Exception as e:
-        # print ('error finding character type:' + str(e) +'\n')
-        print (bcolors.HEADER + "usage:", sys.argv[0].lower() + " <character type>" +bcolors.ENDC + "\n\nValid types are:")
-        for char_type in char_maps.keys():
-            print (bcolors.OKGREEN + char_type + " " + bcolors.ENDC, end="")
+    except IndexError as e_error:
+        # print ('error finding character type:' + str(e_error) +'\n')
+        print ("%s%s%s%s%s%s" % (Colors.c_header,"usage:",sys.argv[0].lower()," <character type>",Colors.end_c,"\n\nValid types are:"))
+        for char_type in char_maps:
+            print ("%s%s%s%s" % (Colors.ok_green,char_type," ",Colors.end_c), end="")
         print ("\n")
         sys.exit()
 
